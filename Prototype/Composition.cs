de56af430f9ebf3a813a28994d78dd40ee8a83cf
Shane Skiles <shane.skiles@gmail.com>
2026-07-23 21:29:01 -0500

    Comp concept

--- Prototype/Composition.cs
+++ Prototype/Composition.cs
@@ -10,18 +10,26 @@ using MyActionTask = Task<Domain.Result<MyActionResult, Exception>>;
 public static class Composition
 {
     private static string _connectionString = default!;
-
+    /*
     private static Func<IDbConnection> newConnection = default!;
     private static Func<MyActionRepoData, CancellationToken, MyActionTask> myRepoAction = default!;
     private static Func<MyActionWebData, CancellationToken, MyActionTask> myServiceAction = default!;
+    */
+    private static Func<IDbConnection> NewConnection { get => () => new SqliteConnection(_connectionString); }
 
-    public static Func<MyActionWebData, CancellationToken, MyActionTask> ServiceAction { get => myServiceAction; }
+    private static Func<MyActionRepoData, CancellationToken, MyActionTask> MyRepoAction { get  =>
+            async (data, cancellationToken) =>
+                await UserRepository.MyActionAsync(NewConnection, data, cancellationToken); }
+
+    public static Func<MyActionWebData, CancellationToken, MyActionTask> ServiceAction { get =>
+            async (data, cancellationToken) =>
+                await UserService.MyAction(MyRepoAction, data, cancellationToken); }
 
     public static async Task<IApplicationBuilder> RegisterAllEndpoints(
         this WebApplication app, IConfiguration configuration)
     {
         _connectionString = configuration.GetConnectionString("Sqlite")!;
-
+        /*
         newConnection = () => new SqliteConnection(_connectionString);
 
         myRepoAction = async (data, cancellationToken) =>
@@ -29,7 +37,7 @@ public static class Composition
 
         myServiceAction = async (data, cancellationToken) =>
                 await UserService.MyAction(myRepoAction, data, cancellationToken);
-        /*
+        
         Func<IDbConnection> newConnection = () => new SqliteConnection(_connectionString);
 
         Func<MyActionRepoData, CancellationToken, MyActionTask> myRepoAction =
